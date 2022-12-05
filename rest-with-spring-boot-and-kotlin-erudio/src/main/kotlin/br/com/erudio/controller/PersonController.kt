9cb58042a8162efdfa1bb0d69c96c4a7d24b545d
WesNinaja <wesleyninaja@gmail.com>
2022-12-04 23:53:53 -0300

    feat: Configuring VO with Dozer to service

--- rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/controller/PersonController.kt
+++ rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/controller/PersonController.kt
@@ -1,6 +1,6 @@
 package br.com.erudio.controller
 
-import br.com.erudio.model.Person
+import br.com.erudio.data.vo.v1.PersonVO
 import br.com.erudio.service.PersonService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
@@ -20,29 +20,29 @@ class PersonController {
 
 
     @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun findAll(): List<Person> {
+    fun findAll(): List<PersonVO> {
         return service.findAll()
 
     }
 
     @GetMapping(value = ["/{id}"], produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun findById(@PathVariable(value = "id") id: Long): Person {
+    fun findById(@PathVariable(value = "id") id: Long): PersonVO {
         return service.findById(id)
 
     }
 
     @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE],
             produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun create(@RequestBody person: Person): Person {
+    fun create(@RequestBody PersonVO: PersonVO): PersonVO {
 
-        return service.create(person)
+        return service.create(PersonVO)
     }
 
     @PutMapping(consumes = [MediaType.APPLICATION_JSON_VALUE],
             produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun update(@RequestBody person: Person): Person {
+    fun update(@RequestBody PersonVO: PersonVO): PersonVO {
 
-        return service.update(person)
+        return service.update(PersonVO)
     }
 
     @DeleteMapping(value = ["/{id}"], produces = [MediaType.APPLICATION_JSON_VALUE])
--- rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/service/PersonService.kt
+++ rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/service/PersonService.kt
@@ -1,6 +1,8 @@
 package br.com.erudio.service
 
+import br.com.erudio.data.vo.v1.PersonVO
 import br.com.erudio.exception.ResourceNotFoundException
+import br.com.erudio.mapper.DozerMapper
 import br.com.erudio.model.Person
 import br.com.erudio.repository.PersonRepository
 import org.springframework.beans.factory.annotation.Autowired
@@ -15,24 +17,27 @@ class PersonService {
 
     private val logger = Logger.getLogger(PersonService::class.java.name)
 
-    fun findAll(): List<Person> {
+    fun findAll(): List<PersonVO> {
         logger.info("Finding all people!")
-        return repository.findAll()
+        val persons = repository.findAll()
+        return DozerMapper.parseListObjects(persons, PersonVO::class.java)
     }
 
 
-    fun findById(id : Long) : Person{
+    fun findById(id : Long) : PersonVO{
         logger.info("finding one person")
-        return repository.findById(id)
+        val person = repository.findById(id)
                 .orElseThrow { ResourceNotFoundException("No records found for this ID!") }
+        return DozerMapper.parseObject(person, PersonVO::class.java)
     }
 
-    fun create(person : Person) : Person {
+    fun create(person : PersonVO) : PersonVO {
         logger.info("Creating one person with name ${person.firstName}!")
-        return repository.save(person)
+        val entity : Person = DozerMapper.parseObject(person, Person::class.java)
+        return DozerMapper.parseObject(repository.save(entity), PersonVO::class.java)
     }
 
-    fun update(person : Person) : Person {
+    fun update(person : PersonVO) : PersonVO {
         logger.info("Updating one person with id ${person.id}!")
         val entity = repository.findById(person.id)
                 .orElseThrow { ResourceNotFoundException("No records found for this ID!") }
@@ -42,7 +47,7 @@ class PersonService {
         entity.address = person.address
         entity.gender = person.gender
 
-        return repository.save(entity)
+        return DozerMapper.parseObject(repository.save(entity), PersonVO::class.java)
     }
 
     fun delete(id: Long) {
